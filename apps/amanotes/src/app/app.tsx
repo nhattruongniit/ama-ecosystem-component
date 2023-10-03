3988f136dccf1ed97f58d84a25c05bacfc54fdba
truong.nn <truong.nn@amanotes.com>
2023-10-03 15:29:54 +0700

    remove theme

--- apps/amanotes/src/app/app.tsx
+++ apps/amanotes/src/app/app.tsx
@@ -1,14 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
-
-import { ThemePiano } from '@ama-theme/piano'
 
 export function App() {
   return (
     <div>
-      <ThemePiano />
       <NxWelcome title="amanotes" />
     </div>
   );
